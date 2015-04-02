12ecd0655887916aa18ed52ce3e5b86b73b173e8
ElezStefan <elez43513ei@fon.bg.ac.rs>
2015-04-02 02:56:44 +0200

    Dodate toString i Equals metode u klase Autor i Knjiga

--- Biblioteka/src/biblioteka/Autor.java
+++ Biblioteka/src/biblioteka/Autor.java
@@ -5,4 +5,36 @@ import java.util.LinkedList;
 public class Autor {
 
 	private LinkedList <Autor> autori = new LinkedList<Autor>();
+
+	@Override
+	public String toString() {
+		return "Autor [autori=" + autori + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((autori == null) ? 0 : autori.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Autor other = (Autor) obj;
+		if (autori == null) {
+			if (other.autori != null)
+				return false;
+		} else if (!autori.equals(other.autori))
+			return false;
+		return true;
+	}
+	
+	
 }
--- Biblioteka/src/biblioteka/Knjiga.java
+++ Biblioteka/src/biblioteka/Knjiga.java
@@ -37,6 +37,58 @@ public class Knjiga {
 	public void setIzdanje(String izdanje) {
 		this.izdanje = izdanje;
 	}
+	@Override
+	public String toString() {
+		return "Knjiga [naslov=" + naslov + ", ISBN=" + ISBN + ", autor="
+				+ autor + ", izdavac=" + izdavac + ", izdanje=" + izdanje + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((ISBN == null) ? 0 : ISBN.hashCode());
+		result = prime * result + ((autor == null) ? 0 : autor.hashCode());
+		result = prime * result + ((izdanje == null) ? 0 : izdanje.hashCode());
+		result = prime * result + ((izdavac == null) ? 0 : izdavac.hashCode());
+		result = prime * result + ((naslov == null) ? 0 : naslov.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Knjiga other = (Knjiga) obj;
+		if (ISBN == null) {
+			if (other.ISBN != null)
+				return false;
+		} else if (!ISBN.equals(other.ISBN))
+			return false;
+		if (autor == null) {
+			if (other.autor != null)
+				return false;
+		} else if (!autor.equals(other.autor))
+			return false;
+		if (izdanje == null) {
+			if (other.izdanje != null)
+				return false;
+		} else if (!izdanje.equals(other.izdanje))
+			return false;
+		if (izdavac == null) {
+			if (other.izdavac != null)
+				return false;
+		} else if (!izdavac.equals(other.izdavac))
+			return false;
+		if (naslov == null) {
+			if (other.naslov != null)
+				return false;
+		} else if (!naslov.equals(other.naslov))
+			return false;
+		return true;
+	}
 	
 	
 }
