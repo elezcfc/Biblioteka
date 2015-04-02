e359ce5c25cfc5152ff8599426e8191bd9c55386
ElezStefan <elez43513ei@fon.bg.ac.rs>
2015-04-02 02:47:05 +0200

    Dodati atributi, getteri, setteri u klasu Knjiga

--- Biblioteka/src/biblioteka/Knjiga.java
+++ Biblioteka/src/biblioteka/Knjiga.java
@@ -2,4 +2,41 @@ package biblioteka;
 
 public class Knjiga {
 
+	public String naslov;
+	public String ISBN;
+	public String autor;
+	public String izdavac;
+	public String izdanje;
+	public String getNaslov() {
+		return naslov;
+	}
+	public void setNaslov(String naslov) {
+		this.naslov = naslov;
+	}
+	public String getISBN() {
+		return ISBN;
+	}
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public String getIzdavac() {
+		return izdavac;
+	}
+	public void setIzdavac(String izdavac) {
+		this.izdavac = izdavac;
+	}
+	public String getIzdanje() {
+		return izdanje;
+	}
+	public void setIzdanje(String izdanje) {
+		this.izdanje = izdanje;
+	}
+	
+	
 }
